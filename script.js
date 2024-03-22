0ee84051f9ca14267053ee8da675083b060de163
Amaurirj <amaurietelvino@gmail.com>
2024-03-21 21:37:50 -0300

    subindo o projeto

---
+++ script.js
@@ -0,0 +1,50 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+const btnCopiar = document.querySelector(".btn-copiar");
+
+function btnEncriptar() {
+    const textoEncriptado = criptografar(textArea.value);
+    mensagem.value = textoEncriptado;
+    checkTextArea();
+    textArea.value = "";
+}
+function criptografar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; // usando Array
+    
+    stringEncriptada = stringEncriptada.toLowerCase(); // retorna a string sempre em letras minusculas
+    stringEncriptada = stringEncriptada.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    //remove acentos e caracteres especiais de uma string
+
+    for(let i = 0; i < matrizCodigo.length; i++) { // for(let i=0; - Usada para criar a variável de controle de loop
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return(stringEncriptada);
+}
+function bntDesencriptar() {
+    const textoDesencriptado = descriptografar(textArea.value); //const - uso de para indicar uma variavel constante
+    mensagem.value = textoDesencriptado;
+    checkTextArea();
+    textArea.value = "";
+}
+function descriptografar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; // let - usado para indicar uma variavel onde poderá haver mudanças.
+    
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+    stringDesencriptada = stringDesencriptada.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return(stringDesencriptada);
+}
+function copiarTexto() { // Funçãi utilizada para copiar o texto dentro de um navegador web.
+    navigator.clipboard.writeText(mensagem.value).then(() => { // linha utilizada em copiar para área de trsnferencia.
+        alert('Copiado para a área de tranferência'); // exibir pop-up na tela indicando o que foi copiado.
+    })
+}
